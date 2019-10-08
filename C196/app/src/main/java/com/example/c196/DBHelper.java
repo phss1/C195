b626cb7c65cba759efc5f5ce69fbd88ceff4553c
unknown <pheiss1@wgu.edu>
2019-10-08 16:38:04 -0700

    C196 - Saving changes 10/08 1638

---
+++ C196/app/src/main/java/com/example/c196/DBHelper.java
@@ -0,0 +1,29 @@
+package com.example.c196;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper
+{
+    private static final String DATABASE_NAME = "C196.DB";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public DBHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
+    {
+
+    }
+}
--- C196/app/src/main/java/com/example/c196/MainActivity.java
+++ C196/app/src/main/java/com/example/c196/MainActivity.java
@@ -3,14 +3,31 @@ package com.example.c196;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    DBHelper myHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myHelper = new DBHelper(MainActivity.this);
+
+        myHelper.getWritableDatabase();
+
+        Toast.makeText(MainActivity.this, myHelper.getDatabaseName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        myHelper.close();
+        Toast.makeText(MainActivity.this, myHelper.getDatabaseName() + "closed!", Toast.LENGTH_SHORT).show();
     }
 }
