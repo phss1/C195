117416c8a34d939f4b49383799fb66acdaa30557
unknown <pheiss1@wgu.edu>
2019-10-08 14:25:39 -0700

    C196 - Saving changes 10/08 1425

--- C196/app/src/androidTest/java/com/example/c196/ExampleInstrumentedTest.java
+++ C196/app/src/androidTest/java/com/example/c196/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.C196_proj;
+package com.example.c196;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.C196_proj", appContext.getPackageName());
+        assertEquals("com.example.c196", appContext.getPackageName());
     }
 }
--- C196/app/src/main/java/com/example/c196/MainActivity.java
+++ C196/app/src/main/java/com/example/c196/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.C196_proj;
+package com.example.c196;
 
 import androidx.appcompat.app.AppCompatActivity;
 
